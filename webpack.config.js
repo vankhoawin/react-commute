81687f7693af212742f00d128b3ce378e43e3724
Van Nguyen <vnguyen94@gmail.com>
2015-10-27 18:12:14 -0700

    add bootstrap-sass and loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,12 @@ var devFlagPlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
 });
 
+var bootstrapPath = path.resolve(__dirname, "./node_modules/bootstrap-sass/assets/stylesheets");
+
 module.exports = {
   devtool: 'eval',
   entry: [
+    'bootstrap-sass!./bootstrap-sass.config.js',
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
     './src/index'
@@ -20,6 +23,10 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
     devFlagPlugin
   ],
   module: {
@@ -29,10 +36,14 @@ module.exports = {
         loader: 'react-hot!babel',
         include: path.join(__dirname, 'src')
       },
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-      }
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+      { test: /\.scss$/, loader: "style!css!sass?outputStyle=expanded" },
+
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
     ]
   }
 };
