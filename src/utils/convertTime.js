21ec9f7e22104ec2ec202da3a000e03674a4d367
Van Nguyen <vnguyen94@gmail.com>
2015-10-27 22:19:55 -0700

    lint

--- src/utils/convertTime.js
+++ src/utils/convertTime.js
@@ -1,17 +1,17 @@
 export function toSeconds(time) {
-    let parts = time.split(':');
-    return (+parts[0])*3600 + (+parts[1])*60 + (+parts[2]);
+  let parts = time.split(':');
+  return (+parts[0])*3600 + (+parts[1])*60 + (+parts[2]);
 }
 
 export function toHHMMSS(sec) {
-    let sec_num = parseInt(sec, 10);
-    let hours = Math.floor(sec_num / 3600);
-    let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    let seconds = sec_num - (hours * 3600) - (minutes * 60);
+  let sec_num = parseInt(sec, 10);
+  let hours = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    let time = hours + ':' + minutes + ':' + seconds;
-    return time;
+  if (hours   < 10) {hours   = '0'+hours;}
+  if (minutes < 10) {minutes = '0'+minutes;}
+  if (seconds < 10) {seconds = '0'+seconds;}
+  let time = hours + ':' + minutes + ':' + seconds;
+  return time;
 }
